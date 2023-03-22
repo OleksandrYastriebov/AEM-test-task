0f31c032a83644d4745adb1f5a71a61ae30d8476
Oleksandr Yastriebov <oleksandr_yastriebov@epam.com>
2023-03-22 21:00:22 +0200

    Refactor AemPracticeModel

--- core/src/main/java/com/site/model/AemPracticeModel.java
+++ core/src/main/java/com/site/model/AemPracticeModel.java
@@ -2,34 +2,32 @@ package com.site.model;
 
 import com.site.services.RunModeService;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import javax.inject.Inject;
 import java.util.List;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class AemPracticeModel {
 
     @OSGiService
     private RunModeService runModeService;
 
     @ChildResource
-    @Optional
     private List<NavigationItem> navigationItems;
 
-    @Inject
-    @Optional
+    @ValueMapValue
     private String imagePath;
 
-    @Inject
-    @Optional
+    @ValueMapValue
+    @Default(booleanValues = {false})
     private boolean isBackgroundImage;
 
-    @Inject
-    @Optional
+    @ValueMapValue
     private String title;
 
     public List<NavigationItem> getNavigationItems() {
